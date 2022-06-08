b602adee064dd552043a5920ed86365cf7afbfd3
Alfman247 <84677298+Alfman247@users.noreply.github.com>
2022-06-08 12:53:12 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,69 @@
+input.onButtonPressed(Button.A, function () {
+    if (Mode_0__setup_1__question == 0) {
+        Question_number += 1
+    } else {
+        if (Question_number == 1) {
+            Incorrect()
+        } else if (Question_number == 2) {
+            Correct()
+        } else if (Question_number == 3) {
+            Incorrect()
+        } else if (Question_number == 4) {
+            Incorrect()
+        } else if (Question_number == 5) {
+            Correct()
+        }
+    }
+})
+function setup_mode () {
+    Mode_0__setup_1__question = 0
+    Question_number = 0
+}
+input.onButtonPressed(Button.B, function () {
+    if (Mode_0__setup_1__question == 0) {
+        question_mode()
+        Cool_animation()
+    } else {
+        if (Question_number == 1) {
+            Correct()
+        } else if (Question_number == 2) {
+            Incorrect()
+        } else if (Question_number == 3) {
+            Correct()
+        } else if (Question_number == 4) {
+            Correct()
+        } else if (Question_number == 5) {
+            Incorrect()
+        }
+    }
+})
+function question_mode () {
+    Mode_0__setup_1__question = 1
+}
+function Cool_animation () {
+    basic.showIcon(IconNames.Square)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(100)
+    basic.clearScreen()
+}
+function Incorrect () {
+    basic.showIcon(IconNames.Yes)
+    music.playMelody("C5 B A G F E D C ", 120)
+    basic.pause(100)
+}
+function Correct () {
+    basic.showIcon(IconNames.Yes)
+    music.playMelody("C D E F G A B C5 ", 120)
+    basic.pause(100)
+}
+let Question_number = 0
+let Mode_0__setup_1__question = 0
+setup_mode()
+basic.forever(function () {
+    if (Question_number != 0 && Mode_0__setup_1__question == 0) {
+        basic.showNumber(Question_number)
+    }
+})
